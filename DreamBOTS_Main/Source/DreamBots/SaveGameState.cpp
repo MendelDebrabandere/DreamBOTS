76f9e915f4a3ead7e44bf4f509674818b10297f4
Mendel Debrabandere <mendel.debrabandere@hotmail.com>
2023-12-27 08:31:40 +0100

    removing project files

--- DreamBOTS_Main/Source/DreamBots/SaveGameState.cpp
+++
@@ -1,9 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
-#include "SaveGameState.h"
-
-USaveGameState::USaveGameState()
-{
-	FoundMovieTapes.Init(false, 16);
-}
